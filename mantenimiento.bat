00e79c13aa756b84f553f61be89ae252a072b3a4
Luca-237 <luca.mmorales@gmail.com>
2025-09-16 12:20:51 -0300

    Update mantenimiento.bat

--- mantenimiento.bat
+++ mantenimiento.bat
@@ -5,5 +5,6 @@ start "" "C:\Program Files\Git\bin\bash.exe"
 :: Espera 20 segundos
 timeout /t 7 /nobreak >nul
 
-:: Ejecuta los comandos en Git Bash
+:: Ejecuta los comandos en Git Bash, se debe ubicar los directorios deseados
+
 start "" "C:\Program Files\Git\bin\bash.exe" -c "cd ~/Desktop/programa\ mantenimiento/ && node servidor.js"
